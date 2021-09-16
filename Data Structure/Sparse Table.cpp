92688e1d5ce9cc94ae04ea7bcf21a49ab9ef17f8
Fu_L <65212600+Fu-L@users.noreply.github.com>
2021-09-16 23:11:39 +0900

    Create Sparse Table.cpp

---
+++ Data Structure/Sparse Table.cpp
@@ -0,0 +1,23 @@
+// vector<int> vec(n); SparseTable<int> st(vec); でSparseTableを構築。
+// st.query(l,r); でvec[l]~vec[r-1]のminを求める。minをmax,gcd,lcmに変えることもできる。vecの更新はできない。
+template<typename T>
+struct SparseTable{
+    vector<vector<T>> st;
+    SparseTable(vector<T> &a){
+        int n=a.size();
+        int LOG=32-__builtin_clz(n);
+        st=vector<vector<T>>(LOG,vector<T>(n));
+        rep(i,0,n){
+            st[0][i]=a[i];
+        }
+        rep(i,0,LOG-1){
+            rep(j,0,n-(1<<i)){
+                st[i+1][j]=min(st[i][j],st[i][j+(1<<i)]);
+            }
+        }
+    }
+    T query(int l,int r){
+        int d=31-__builtin_clz(r-l);
+        return min(st[d][l],st[d][r-(1<<d)]);
+    }
+};
