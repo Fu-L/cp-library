7ddcf1e0ed0ba591331f23a13f86118f3ef0b2c8
Fu_L <65212600+Fu-L@users.noreply.github.com>
2021-09-16 22:04:27 +0900

    Create Range Sum Query & Range Update Query.cpp

---
+++ Data Structure/Range Sum Query & Range Update Query.cpp
@@ -0,0 +1,17 @@
+// vector<S> v(N,{0, 1}); lazy_segtree<S,op,e,F,mapping,composition,id> seg(v); で長さnのセグ木を構築。
+// ll ma=seg.prod(l,r).value; でa[l]~a[r-1]のsumを求める。
+// seg.apply(l,r,x); でa[l]~a[r-1]をxに変更。
+struct S{
+    ll value;
+    ll size;
+};
+using F=ll;
+const F ID=8e18;
+S op(S a,S b){return {a.value+b.value,a.size+b.size};}
+S e(){return {0,0};}
+S mapping(F f,S x){
+    if(f!=ID) x.value=f*x.size;
+    return x;
+}
+F composition(F f,F g){return (f==ID?g:f);}
+F id(){return ID;}
