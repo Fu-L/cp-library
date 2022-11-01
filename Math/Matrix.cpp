ae28a5c281c23bbef82bd2c1c62dc383923edd7e
Fu_L <65212600+Fu-L@users.noreply.github.com>
2022-11-02 00:34:45 +0900

    Update Matrix.cpp

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -1,35 +1,36 @@
-template<typename T>
-struct Matrix{
-    int h,w;
+template <typename T>
+struct Matrix {
+    int h, w;
     vector<vector<T>> d;
     Matrix() {}
-    Matrix(int h,int w,T val=0):h(h),w(w),d(h,vector<T>(w,val)) {}
-    Matrix &unit(){
-        assert(h==w);
-        rep(i,0,h) d[i][i]=1;
+    Matrix(int h, int w, T val = 0)
+        : h(h), w(w), d(h, vector<T>(w, val)) {}
+    Matrix &unit() {
+        assert(h == w);
+        rep(i, 0, h) d[i][i] = 1;
         return *this;
     }
-    const vector<T> &operator[](int i)const{
+    const vector<T> &operator[](int i) const {
         return d[i];
     }
-    vector<T> &operator[](int i){
+    vector<T> &operator[](int i) {
         return d[i];
     }
-    Matrix operator*(const Matrix &a)const{
-        assert(w==a.h);
-        Matrix r(h,a.w);
-        rep(i,0,h) rep(k,0,w) rep(j,0,a.w) {
-            r[i][j]+=d[i][k]*a[k][j];
+    Matrix operator*(const Matrix &a) const {
+        assert(w == a.h);
+        Matrix r(h, a.w);
+        rep(i, 0, h) rep(k, 0, w) rep(j, 0, a.w) {
+            r[i][j] += d[i][k] * a[k][j];
         }
         return r;
     }
-    Matrix pow(ll t)const{
-        assert(h==w);
+    Matrix pow(ll t) const {
+        assert(h == w);
         if(!t) return Matrix(h, h).unit();
-        if (t==1) return *this;
-        Matrix r=pow(t>>1);
-        r=r*r;
-        if (t&1) r=r*(*this);
+        if(t == 1) return *this;
+        Matrix r = pow(t >> 1);
+        r = r * r;
+        if(t & 1) r = r * (*this);
         return r;
     }
 };
