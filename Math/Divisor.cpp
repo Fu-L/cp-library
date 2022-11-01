35751e9ee056f48237041d7e9937fd7dee1b9b22
Fu_L <65212600+Fu-L@users.noreply.github.com>
2022-11-02 00:33:47 +0900

    Update Divisor.cpp

--- Math/Divisor.cpp
+++ Math/Divisor.cpp
@@ -1,11 +1,12 @@
-// vector<ll> vec=divisor(n); でnの約数を列挙する。(昇順とは限らない。)
-vector<ll> divisor(ll n){
+// vector<ll> vec = divisor(n); でnの約数を列挙する。(昇順)
+vector<ll> divisor(ll n) {
     vector<ll> res;
-    for(ll i=1;i*i<=n;i++){
-        if(n%i==0){
+    for(ll i = 1; i * i <= n; i++) {
+        if(n % i == 0) {
             res.push_back(i);
-            if(i!=n/i) res.push_back(n/i);
+            if(i != n / i) res.push_back(n / i);
         }
     }
+    sort(res.begin(), res.end());
     return res;
 }
