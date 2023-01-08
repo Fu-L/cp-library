488019415f898626e2e0845dcfcedc6141e72db0
Fu_L <65212600+Fu-L@users.noreply.github.com>
2023-01-08 15:58:32 +0900

    Update Template for AtCoder.cpp

--- Template/Template for AtCoder.cpp
+++ Template/Template for AtCoder.cpp
@@ -2,14 +2,14 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
-typedef long long ll;
-typedef pair<ll,ll> P;
-typedef modint998244353 mint;
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define rrep(i,a,b) for(ll i=a;i>=b;i--)
-const ll inf=1e18;
-int main(void){
+using ll = long long;
+using P = pair<ll, ll>;
+using mint = modint998244353;
+#define rep(i, a, b) for(ll i = a; i < b; i++)
+#define rrep(i, a, b) for(ll i = a; i >= b; i--)
+const ll inf = 4e18;
+int main(void) {
     cin.tie(0);
     ios::sync_with_stdio(0);
-	
+    
 }
