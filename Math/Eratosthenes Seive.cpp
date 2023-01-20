bc3f1e6902b9caabaed0af4752303f983a489f49
Fu_L <65212600+Fu-L@users.noreply.github.com>
2023-01-20 09:43:32 +0900

    Update Eratosthenes Seive.cpp

--- Math/Eratosthenes Seive.cpp
+++ Math/Eratosthenes Seive.cpp
@@ -1,19 +1,15 @@
-// ll p=seive(n); でn以下の素数を列挙する。pはn以下の素数の個数、prime[i]はi番目の素数、is_prime[i]はiが素数かどうかを表す。
-ll prime[MAX_N];
-bool is_prime[MAX_N];
-ll seive(ll n){
-    ll p=0;
-    for(ll i=0;i<=n;i++){
-        is_prime[i]=true;
-    }
-    is_prime[0]=is_prime[1]=false;
-    for(ll i=2;i<=n;i++){
-        if(is_prime[i]){
-            prime[p++]=i;
-            for(ll j=2*i;j<=n;j+=i){
-                is_prime[j]=false;
+// seive(n); でn以下の素数を列挙する。prime[i]はi番目の素数、is_prime[i]はiが素数かどうかを表す。
+vector<ll> prime;
+vector<bool> is_prime;
+void seive(ll n) {
+    is_prime.resize(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    rep(i, 2, n + 1) {
+        if(is_prime[i]) {
+            prime.push_back(i);
+            for(ll j = 2 * i; j <= n; j += i) {
+                is_prime[j] = false;
             }
         }
     }
-    return p;
 }
