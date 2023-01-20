dbd74bc7d62d3c787ff745777cd4f5d479928522
Fu_L <65212600+Fu-L@users.noreply.github.com>
2023-01-20 12:44:41 +0900

    Update Rotate(vector<vector<T>>).cpp

--- Data Structure/Rotate(vector<vector<T>>).cpp
+++ Data Structure/Rotate(vector<vector<T>>).cpp
@@ -1,10 +1,10 @@
-template<typename T>
-vector<vector<T>> rotate(vector<vector<T>> vec){
-    int n=vec.size(),m=vec[0].size();
-    vector<vector<T>> res(m,vector<T>(n));
-    rep(i,0,n){
-        rep(j,0,m){
-            res[j][n-1-i]=vec[i][j];
+template <typename T>
+vector<vector<T>> rotate(vector<vector<T>> vec) {
+    int n = vec.size(), m = vec[0].size();
+    vector<vector<T>> res(m, vector<T>(n));
+    rep(i, 0, n) {
+        rep(j, 0, m) {
+            res[j][n - 1 - i] = vec[i][j];
         }
     }
     return res;
