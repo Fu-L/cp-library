e7738ce1fa9719006c6fb6bf2034eeb8f8b33ff4
Fu_L <65212600+Fu-L@users.noreply.github.com>
2023-01-20 12:48:06 +0900

    Update Range Maximum Query & Range Add Query.cpp

--- Data Structure/Range Maximum Query & Range Add Query.cpp
+++ Data Structure/Range Maximum Query & Range Add Query.cpp
@@ -1,11 +1,21 @@
 // lazy_segtree<S,op,e,F,mapping,composition,id> seg(n); で長さnのセグ木を構築。
 // ll ma=seg.prod(l,r); でa[l]~a[r-1]のmaxを求める。
 // seg.apply(l,r,x); でa[l]~a[r-1]にxを加える。
-using S=ll;
-using F=ll;
-const S INF=8e18;
-S op(S a,S b){return max(a,b);}
-S e(){return -INF;}
-S mapping(F f,S x){return f+x;}
-F composition(F f,F g){return f+g;}
-F id(){return 0;}
+using S = ll;
+using F = ll;
+const S INF = 8e18;
+S op(S a, S b) {
+    return max(a, b);
+}
+S e() {
+    return -INF;
+}
+S mapping(F f, S x) {
+    return f + x;
+}
+F composition(F f, F g) {
+    return f + g;
+}
+F id() {
+    return 0;
+}
