2182bae0031049eeb7146f3c3e776a7384cbfc3f
Fu_L <65212600+Fu-L@users.noreply.github.com>
2023-01-28 00:38:08 +0900

    Update Runge-Kutta.cpp

--- Math/Runge-Kutta.cpp
+++ Math/Runge-Kutta.cpp
@@ -1,77 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Data {
-    // 持ちたいデータ。
-    double x, y, vx, vy;
-    // 加法演算の定義。ベクトルの成分ごとに加算。
-    Data& operator+=(const Data& d) {
-        (*this).x += d.x;
-        (*this).y += d.y;
-        (*this).vx += d.vx;
-        (*this).vy += d.vy;
-        return (*this);
+struct RungeKutta : vector<double> {
+    using vector<double>::vector;
+    RungeKutta& operator+=(const RungeKutta& v) {
+        for(int i = 0; i < (int)this->size(); i++) {
+            (*this)[i] += v[i];
+        }
+        return *this;
     }
-    Data operator+(const Data& d) {
-        return (Data(*this) += d);
+    RungeKutta operator+(const RungeKutta& v) {
+        return (RungeKutta)(*this) += v;
     }
-    // 乗法演算の定義。ベクトルの全成分を定数倍。
-    Data& operator*=(const double& k) {
-        (*this).x *= k;
-        (*this).y *= k;
-        (*this).vx *= k;
-        (*this).vy *= k;
-        return (*this);
+    RungeKutta& operator*=(const double& k) {
+        for(int i = 0; i < (int)this->size(); i++) {
+            (*this)[i] *= k;
+        }
+        return *this;
     }
-    Data operator*(const double& k) {
-        return (Data(*this) *= k);
+    RungeKutta operator*(const double& k) {
+        return (RungeKutta)(*this) *= k;
+    }
+    RungeKutta func() const;
+    vector<RungeKutta> calc(double dt, int period) {
+        vector<RungeKutta> Y;
+        Y.push_back(*this);
+        for(int i = 0; i < period; i++) {
+            RungeKutta k1 = Y.back().func();
+            RungeKutta k2 = (Y.back() + k1 * (dt / 2.0)).func();
+            RungeKutta k3 = (Y.back() + k2 * (dt / 2.0)).func();
+            RungeKutta k4 = (Y.back() + k3 * dt).func();
+            Y.push_back(Y.back() + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (dt / 6.0));
+        }
+        return Y;
     }
 };
-// 微分方程式f(X)の定義。
-Data func(Data X) {
-    Data res;
-    res.x = X.vx;
-    res.y = X.vy;
-    res.vx = X.vy;
-    res.vy = -X.vx;
+RungeKutta RungeKutta::func() const {
+    const int n = this->size();
+    RungeKutta res(n);
+    // To do 微分方程式f(X)の定義 [0] : x, [1] : y, [2] : vx, [3] : vy
+    res[0] = (*this)[2];
+    res[1] = (*this)[3];
+    res[2] = (*this)[3];
+    res[3] = -(*this)[2];
     return res;
 }
-void Euler(vector<Data>& Y, double h, int period) {
-    for(int i = 0; i < period; i++) {
-        Data k = func(Y[i]);
-        Y[i + 1] = Y[i] + k * h;
-    }
-}
-void Heun(vector<Data>& Y, double h, int period) {
-    for(int i = 0; i < period; i++) {
-        Data k1 = func(Y[i]);
-        Data k2 = func(Y[i] + k1 * h);
-        Y[i + 1] = Y[i] + (k1 + k2) * (h / 2.0);
-    }
-}
-void RK4(vector<Data>& Y, double h, int period) {
-    for(int i = 0; i < period; i++) {
-        Data k1 = func(Y[i]);
-        Data k2 = func(Y[i] + k1 * (h / 2.0));
-        Data k3 = func(Y[i] + k2 * (h / 2.0));
-        Data k4 = func(Y[i] + k3 * h);
-        Y[i + 1] = Y[i] + (k1 + k2 * 2.0 + k3 * 2.0 + k4) * (h / 6.0);
-    }
-}
 int main(void) {
-    // 定数などの宣言。
+    // To do 定数などの宣言
     const double m = 1.0;
     const double q = 1.0;
     const double B = 1.0;
     const double tau = 2.0 * M_PI * m / (q * B);
     const int period = 64;
     const double dt = tau / period;
-    vector<Data> Y(period + 1);
-    Y[0] = {-1.0, 0.0, 0.0, 1.0};
-    // Euler(Y, dt, period);
-    // Heun(Y, dt, period);
-    RK4(Y, dt, period);
+    // To do 初期値の設定
+    RungeKutta Y_0 = {-1.0, 0.0, 0.0, 1.0};
+    // To do 時間幅と繰り返し回数を指定
+    vector<RungeKutta> Y = Y_0.calc(dt, period);
     for(int i = 0; i <= period; i++) {
-        cout << fixed << setprecision(15) << Y[i].x << ' ' << Y[i].y << '\n';
+        cout << fixed << setprecision(15) << Y[i][0] << ' ' << Y[i][1] << '\n';
     }
     return 0;
 }
