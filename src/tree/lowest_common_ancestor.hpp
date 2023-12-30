e08c3d760d0fa45473c8d57480b865e37bf02b3b
Fu-L <nishifu1106@icloud.com>
2023-12-31 01:05:23 +0900

    木の整備

---
+++ src/tree/lowest_common_ancestor.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include "cp-library/src/template/template.hpp"
+#include "cp-library/src/graph/graph_template.hpp"
+template <typename T = int>
+struct LowestCommonAncestor {
+    LowestCommonAncestor(const Graph<T>& g, int root = 0) {
+        assert(0 <= root and root < (int)g.size());
+        init(g, root);
+    }
+    int depth(int v) const {
+        assert(0 <= v and v < n);
+        return dep[v];
+    }
+    T cost(int v) const {
+        assert(0 <= v and v < n);
+        return co[v];
+    }
+    int parent(int v, int x = 1) const {
+        assert(0 <= v and v < n);
+        assert(x >= 0);
+        if(x > dep[v]) return -1;
+        for(int i = 0; x > 0; ++i) {
+            if(x & 1) v = par[i][v];
+            x >>= 1;
+        }
+        return v;
+    }
+    int lca(int u, int v) const {
+        assert(0 <= u and u < n and 0 <= v and v < n);
+        if(dep[u] > dep[v]) swap(u, v);
+        v = parent(v, dep[v] - dep[u]);
+        if(u == v) return u;
+        for(int i = ssize(par) - 1; i >= 0; --i) {
+            if(par[i][u] != par[i][v]) {
+                u = par[i][u];
+                v = par[i][v];
+            }
+        }
+        return par[0][u];
+    }
+    int dist(int u, int v) const {
+        assert(0 <= u and u < n and 0 <= v and v < n);
+        return dep[u] + dep[v] - 2 * dep[lca(u, v)];
+    }
+    T length(int u, int v) const {
+        assert(0 <= u and u < n and 0 <= v and v < n);
+        return co[u] + co[v] - 2 * co[lca(u, v)];
+    }
+
+   private:
+    int n;
+    vector<vector<int>> par;
+    vector<int> dep;
+    vector<T> co;
+    void init(const Graph<T>& g, int root = 0) {
+        n = (int)g.size();
+        int h = 1;
+        while((1 << h) < n) ++h;
+        par.assign(h, vector<int>(n, -1));
+        dep.assign(n, -1);
+        co(n, -1);
+        dfs(g, root, -1, 0, 0);
+        for(int i = 0; i + 1 < ssize(par); ++i) {
+            for(int v = 0; v < n; ++v) {
+                if(par[i][v] != -1) {
+                    par[i + 1][v] = par[i][par[i][v]];
+                }
+            }
+        }
+    }
+    void dfs(const Graph<T>& g, int v, int p, int d, T c) {
+        par[0][v] = p;
+        dep[v] = d;
+        co[v] = c;
+        for(const auto& e : g[v]) {
+            if(e.to == p) continue;
+            dfs(g, e.to, v, d + 1, c + e.cost);
+        }
+    }
+};
---
+++ src/tree/tree_diameter.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "cp-library/src/template/template.hpp"
+#include "cp-library/src/graph/graph_template.hpp"
+template <typename T = int>
+pair<T, vector<int>> tree_diameter(const Graph<T>& g) {
+    int n = (int)g.size();
+    vector<T> depth(n);
+    vector<int> par(n);
+    auto dfs = [&](auto& dfs, int v, int p, T d) -> void {
+        depth[v] = d;
+        par[v] = p;
+        for(auto& e : g[v]) {
+            if(e.to == p) continue;
+            dfs(dfs, e.to, v, d + e.cost);
+        }
+    };
+    int s = 0;
+    for(int i = 0; i < 2; ++i) {
+        dfs(dfs, s, -1, 0);
+        T ma = -1;
+        for(int j = 0; j < n; ++j) {
+            if(depth[j] > ma) {
+                ma = depth[j];
+                s = j;
+            }
+        }
+    }
+    vector<int> path;
+    T diameter = depth[s];
+    while(s != -1) {
+        path.push_back(s);
+        s = par[s];
+    }
+    return {diameter, path};
+}
