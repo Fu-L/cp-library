5086bcf0d8efcb4cc2c773a3e11726784277f6a3
Fu-L <nishifu1106@icloud.com>
2024-01-01 14:50:34 +0900

    hoge

---
+++ src/data_structure/cumulative_sum_2d.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "src/template/template.hpp"
+template <typename T>
+struct CumulativeSum2D {
+    CumulativeSum2D(int H, int W)
+        : h(H), w(W), data(H + 1, vector<T>(W + 1, 0)) {}
+    void add(int i, int j, T x) {
+        assert(0 <= i and i < h);
+        assert(0 <= j and j < w);
+        data[i + 1][j + 1] += x;
+    }
+    void build() {
+        for(int i = 1; i < (int)data.size(); ++i) {
+            for(int j = 1; j < (int)data[i].size(); ++j) {
+                data[i][j] += data[i][j - 1] + data[i - 1][j] - data[i - 1][j - 1];
+            }
+        }
+    }
+    T sum(int li, int lj, int ri, int rj) {
+        assert(0 <= li and li <= ri and ri <= h);
+        assert(0 <= lj and lj <= rj and rj <= w);
+        return data[ri][rj] - data[li][rj] - data[ri][lj] + data[li][lj];
+    }
+    T get(int i, int j) {
+        assert(0 <= i and i < h);
+        assert(0 <= j and j < w);
+        return data[i + 1][j + 1] - data[i][j + 1] - data[i + 1][j] + data[i][j];
+    }
+
+   private:
+    int h, w;
+    vector<vector<T>> data;
+};
