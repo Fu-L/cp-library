e31df78d852cc74859d0cb6f4df72824636b23c2
Fu-L <nishifu1106@icloud.com>
2024-01-03 23:04:37 +0900

    木の直径のテスト

--- verify/aizu_online_judge/grl/diameter_of_a_tree.test.cpp
+++ verify/aizu_online_judge/grl/diameter_of_a_tree.test.cpp
@@ -1,19 +1,16 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_5_A"
 #include "../../../src/template/template.hpp"
 #include "../../../src/graph/graph_template.hpp"
 #include "../../../src/tree/tree_diameter.hpp"
 int main(void) {
     int n;
     cin >> n;
-    Graph<ll> g(n);
+    Graph<int> g(n);
     rep(i, 0, n - 1) {
         int a, b, c;
         cin >> a >> b >> c;
         g.add_edge(a, b, c);
     }
     auto [d, path] = tree_diameter(g);
-    cout << d << ' ' << path.size() << '\n';
-    for(const auto& p : path) {
-        cout << p << '\n';
-    }
+    cout << d << '\n';
 }
