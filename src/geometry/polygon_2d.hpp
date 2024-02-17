2f5d8ec1ff6ff03d3e3d6d93f0bfefb6bb7b49b6
Fu-L <nishifu1106@icloud.com>
2024-02-18 02:29:29 +0900

    多角形のコードの修正

--- src/geometry/polygon_2d.hpp
+++ src/geometry/polygon_2d.hpp
@@ -36,28 +36,46 @@ vector<Point> convex_hull(vector<Point> ps) {
     if(n == 1) return ps;
     vector<Point> ch(2 * n);
     for(int i = 0; i < n; ch[k++] = ps[i++]) {
-        while(k >= 2 and sign(cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1])) <= 0) {
+        while(k >= 2 and sign(cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1])) == -1) {
             --k;
         }
     }
     for(int i = n - 2, t = k + 1; i >= 0; ch[k++] = ps[i--]) {
-        while(k >= t and sign(cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1])) <= 0) {
+        while(k >= t and sign(cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1])) == -1) {
             --k;
         }
     }
     ch.resize(k - 1);
     return ch;
 }
+Real convex_diameter(const vector<Point>& ps) {
+    int n = ps.size(), is = 0, js = 0;
+    for(int i = 1; i < n; ++i) {
+        if(sign(ps[i].imag() - ps[is].imag()) == 1) is = i;
+        if(sign(ps[i].imag() - ps[js].imag()) == -1) js = i;
+    }
+    Real maxdis = norm(ps[is] - ps[js]);
+    int i = is, j = js;
+    do {
+        if(sign(cross(ps[(i + 1) % n] - ps[i], ps[(j + 1) % n] - ps[j])) >= 0) {
+            j = (j + 1) % n;
+        } else {
+            i = (i + 1) % n;
+        }
+        if(norm(ps[i] - ps[j]) > maxdis) {
+            maxdis = norm(ps[i] - ps[j]);
+        }
+    } while(i != is or j != js);
+    return sqrt(maxdis);
+}
 vector<Point> convex_cut(const vector<Point>& ps, const Line& l) {
     int n = ps.size();
     vector<Point> res;
     for(int i = 0; i < n; ++i) {
-        if(!eq(dist_lp(l, ps[i]), 0.0) and !eq(dist_lp(l, ps[i + 1]), 0.0)) {
-            Segment s(ps[i], ps[i + 1]);
-            if(eq(dist_ls(l, s), 0.0)) {
-                auto tmp = intersection_ll(l, s);
-                res.push_back(tmp[0]);
-            }
+        Point cur = ps[i], nex = ps[(i + 1) % n];
+        if(ccw(l.a, l.b, cur) != -1) res.push_back(cur);
+        if(ccw(l.a, l.b, cur) * ccw(l.a, l.b, nex) < 0) {
+            res.push_back(intersection_ll(Line(cur, nex), l)[0]);
         }
     }
     return res;
