9feda6fef5c00bffc0295d702fc1af9ba3e46f02
Fu-L <nishifu1106@icloud.com>
2024-12-25 20:22:39 +0900

    bitsetに対するdebugマクロの修正

--- src/template/debug.hpp
+++ src/template/debug.hpp
@@ -47,7 +47,7 @@ template <size_t N>
 string to_string(bitset<N> v) {
     string res = "";
     for(size_t i = 0; i < N; i++) {
-        res += static_cast<char>('0' + v[i]);
+        res += static_cast<char>('0' + v[N - 1 - i]);
     }
     return res;
 }
